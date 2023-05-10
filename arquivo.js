f9ced19f8fb783d3be920b9782fd33bb7435563d
Cleudio de Faria <cleudio123@gmail.com>
2023-05-10 16:54:42 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,493 @@
+
+// criar uma funcao para o evento de onclick da janela modal
+
+
+
+ 
+
+// ------------------------------------------------------
+
+
+function acao1()
+{
+    let modal = document.querySelector('.quadro01')
+    modal.style.display = 'block'
+
+}
+
+
+function sair1()
+{
+    let sair = document.querySelector('.quadro01')
+    sair.style.display = 'none'
+
+}
+
+
+// ------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 02
+// ............................................
+
+function acao2()
+{
+    let modal = document.querySelector('.quadro02')
+    modal.style.display = 'block'
+
+}
+
+
+function sair2()
+{
+    let sair = document.querySelector('.quadro02')
+    sair.style.display = 'none'
+
+}
+
+
+// ------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 03
+// ............................................
+ 
+function acao3()
+{
+    let modal = document.querySelector('.quadro03')
+    modal.style.display = 'block'
+
+}
+
+
+function sair3()
+{
+    let sair = document.querySelector('.quadro03')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 04
+// ............................................
+ 
+function acao4()
+{
+    let modal = document.querySelector('.quadro04')
+    modal.style.display = 'block'
+
+}
+
+function sair4()
+{
+    let sair = document.querySelector('.quadro04')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 05
+// ............................................
+ 
+function acao5()
+{
+    let modal = document.querySelector('.quadro05')
+    modal.style.display = 'block'
+}
+
+function sair5()
+{
+    let sair = document.querySelector('.quadro05')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 06
+// ............................................
+ 
+function acao6()
+{
+    let modal = document.querySelector('.quadro06')
+    modal.style.display = 'block'
+
+}
+
+
+function sair6()
+{
+    let sair = document.querySelector('.quadro06')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 07
+// ............................................
+ 
+function acao7()
+{
+    let modal = document.querySelector('.quadro07')
+    modal.style.display = 'block'
+
+}
+
+
+function sair7()
+{
+    let sair = document.querySelector('.quadro07')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 08
+// ............................................
+ 
+function acao8()
+{
+    let modal = document.querySelector('.quadro08')
+    modal.style.display = 'block'
+
+}
+
+
+function sair8()
+{
+    let sair = document.querySelector('.quadro08')
+    sair.style.display = 'none'
+
+}
+
+//-------------------------------------------------------
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 09
+// ............................................
+ 
+function acao9()
+{
+    let modal = document.querySelector('.quadro09')
+    modal.style.display = 'block'
+
+}
+
+function sair9()
+{
+    let sair = document.querySelector('.quadro09')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 10
+// ............................................
+ 
+function acao10()
+{
+    let modal = document.querySelector('.quadro100')
+    modal.style.display = 'block'
+
+}
+
+function sair10()
+{
+    let sair = document.querySelector('.quadro100')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 11
+// ............................................
+ 
+function acao11()
+{
+    let modal = document.querySelector('.quadro111')
+    modal.style.display = 'block'
+
+}
+
+function sair11()
+{
+    let sair = document.querySelector('.quadro111')
+    sair.style.display = 'none'
+}
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 12
+// ............................................
+ 
+function acao12()
+{
+    let modal = document.querySelector('.quadro112')
+    modal.style.display = 'block'
+
+}
+
+function sair12()
+{
+    let sair = document.querySelector('.quadro112')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 13
+// ............................................
+ 
+function acao13()
+{
+    let modal = document.querySelector('.quadro113')
+    modal.style.display = 'block'
+
+}
+
+function sair13()
+{
+    let sair = document.querySelector('.quadro113')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 14
+// ............................................
+ 
+function acao14()
+{
+    let modal = document.querySelector('.quadro114')
+    modal.style.display = 'block'
+
+}
+
+function sair14()
+{
+    let sair = document.querySelector('.quadro114')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 15
+// ............................................
+ 
+function acao15()
+{
+    let modal = document.querySelector('.quadro115')
+    modal.style.display = 'block'
+
+}
+
+function sair15()
+{
+    let sair = document.querySelector('.quadro115')
+    sair.style.display = 'none'
+
+}
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 15
+// ............................................
+ 
+function acao16()
+{
+    let modal = document.querySelector('.quadro116')
+    modal.style.display = 'block'
+
+}
+
+function sair16()
+{
+    let sair = document.querySelector('.quadro116')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 17
+// ............................................
+ 
+function acao17()
+{
+    let modal = document.querySelector('.quadro117')
+    modal.style.display = 'block'
+
+}
+
+function sair17()
+{
+    let sair = document.querySelector('.quadro117')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 18
+// ............................................
+ 
+function acao18()
+{
+    let modal = document.querySelector('.quadro118')
+    modal.style.display = 'block'
+
+}
+
+function sair18()
+{
+    let sair = document.querySelector('.quadro118')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 19
+// ............................................
+ 
+function acao19()
+{
+    let modal = document.querySelector('.quadro119')
+    modal.style.display = 'block'
+
+}
+
+function sair19()
+{
+    let sair = document.querySelector('.quadro119')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 20
+// ............................................
+ 
+function acao20()
+{
+    let modal = document.querySelector('.quadro120')
+    modal.style.display = 'block'
+
+}
+
+function sair20()
+{
+    let sair = document.querySelector('.quadro120')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 21
+// ............................................
+ 
+function acao21()
+{
+    let modal = document.querySelector('.quadro121')
+    modal.style.display = 'block'
+
+}
+
+function sair21()
+{
+    let sair = document.querySelector('.quadro121')
+    sair.style.display = 'none'
+
+}
+
+
+
+//-------------------------------------------------------
+
+
+// ...........................................
+//  essa funçao aciona o modal da Imagem 22
+// ............................................
+ 
+function acao22()
+{
+    let modal = document.querySelector('.quadro122')
+    modal.style.display = 'block'
+
+}
+
+function sair22()
+{
+    let sair = document.querySelector('.quadro122')
+    sair.style.display = 'none'
+
+}
+
+
+
+
+
+
+
+
